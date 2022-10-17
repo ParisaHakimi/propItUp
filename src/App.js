bcc4fcb45275715ff2c943d367f782aa452392e7
Parisa Sohrabi <parisahs@gmail.com>
2022-10-17 17:46:35 -0400

    used class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import PersonCard from "./components/PersonCard";
 import SaleItem from "./components/SaleItem";
 function App() {
   return (
     <div className="App">
-      <SaleItem name="Doe, Jane" initialAge="45" hairColor="Black" />
+      {/* <SaleItem name="Doe, Jane" initialAge="45" hairColor="Black" />
       <SaleItem name="Smith, John" initialAge="88" hairColor="Brown" />
       <SaleItem name="Fillmore, Millard" initialAge="50" hairColor="Brown" />
-      <SaleItem name="Smith, Maria" initialAge="62" hairColor="Brown" />
+      <SaleItem name="Smith, Maria" initialAge="62" hairColor="Brown" /> */}
+      <PersonCard name="Smith, Maria" initialAge="62" hairColor="Brown"/>
+      <PersonCard name="Fillmore, Millard" initialAge="50" hairColor="Brown"/>
+      <PersonCard name="Doe, Jane" initialAge="45" hairColor="Black"/>
     </div>
   );
 }
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const PersonCard = () => {
-  return (
-    <div>PersonCard</div>
-  )
+export class PersonCard extends Component {
+  render() {
+    const {name,initialAge,hairColor}= this.props
+    return (
+
+      <div className="container">
+        <h2>{name}</h2>
+        <h3>Age: {initialAge}</h3>
+        <h4>Hair color: {hairColor}</h4>
+       
+      </div>
+    
+    )
+  }
 }
 
-export default PersonCard
+export default PersonCard;
